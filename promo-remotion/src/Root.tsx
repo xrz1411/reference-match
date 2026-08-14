571e37525734ea45249f1c59af36be39fdc6c83f
Richard zhan <zxr@MateBookPro14.local>
2026-08-14 14:17:16 +0800

    Remove promotional video source

--- promo-remotion/src/Root.tsx
+++
@@ -1,4 +0,0 @@
-import './index.css';
-import {MyComposition} from './Composition';
-
-export const RemotionRoot: React.FC = () => <MyComposition />;
--- promo-remotion/src/scenes/PageFade.tsx
+++
@@ -1,9 +0,0 @@
-import {AbsoluteFill, Easing, interpolate, useCurrentFrame} from 'remotion';
-
-const easeOut = Easing.bezier(.16, 1, .3, 1);
-
-export const PageFade: React.FC<{children: React.ReactNode; duration: number}> = ({children, duration}) => {
-	const frame = useCurrentFrame();
-	const opacity = interpolate(frame, [0, 12, duration - 12, duration], [0, 1, 1, 0], {extrapolateLeft: 'clamp', extrapolateRight: 'clamp', easing: easeOut});
-	return <AbsoluteFill style={{opacity}}>{children}</AbsoluteFill>;
-};
